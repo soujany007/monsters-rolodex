9cc402370c4ce8d3aa29c73a1e5d0ce57f1986ee
northout-soujany <soujany.gaur@northout.com>
2020-02-25 18:25:34 +0530

    Updates

---
+++ precache-manifest.819fec0fa8d36819cbb88f6d3e737334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822f73c4fb8d0105b5af848eff59d9ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d8a4d45e74560f78fc97",
+    "url": "/monsters-rolodex/static/css/main.12c2ad43.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a4d45e74560f78fc97",
+    "url": "/monsters-rolodex/static/js/main.2f4bc44c.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
